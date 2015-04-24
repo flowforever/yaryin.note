56426db60c8877d321c865daab151823a57301a1
trump.wang <trump.wang@ringcentral.com>
2015-04-24 17:53:02 +0800

    ng

--- web/public/scripts/app/main.js
+++ web/public/scripts/app/main.js
@@ -1,3 +1,46 @@
 /**
  * Created by trump.wang on 2015/4/24.
  */
+(function () {
+    var app = angular.module('noteApp',[
+        'ngRoute'
+        , 'noteControllers'
+    ]);
+
+    app.config(['$routeProvider', function($routeProvider) {
+    	$routeProvider.when('/list', {
+
+    	})
+    	.when('/edit/:id', {
+
+    	})
+    	.when('/:id', {
+    		controller: 'home'
+    	});
+
+    }]);
+
+    var controllers = angular.module('noteControllers');
+    
+    controllers.controller('home', ['$scope', '$http',
+	function ($scope, $http) {
+
+	}])
+	.controller('edit', ['$scope', '$http',
+	function ($scope, $http) {
+
+	}])
+	.controller('view', ['$scope', '$http',
+	function ($scope, $http) {
+
+	}])
+	.controller('register', ['$scope', '$http',
+	function ($scope, $http) {
+
+	}])
+	.controller('login', ['$scope', '$http',
+	function ($scope, $http) {
+
+	}]);
+
+})();
