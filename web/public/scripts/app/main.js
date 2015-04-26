8ee5fb6285f39bedcab6c308343f67985a0a12bf
Judd Trump <wangzhenchuan@gmail.com>
2015-04-26 10:49:54 +0800

    update new version

--- web/public/scripts/app/main.js
+++ web/public/scripts/app/main.js
@@ -7,14 +7,19 @@
         return location.hash.replace('#', '');
     };
 
+    function navigateToNewFile(fileName) {
+        fileName = fileName || ( new Date().getTime() ).toString(32);
+        document.location.href = '#' + fileName;
+    }
+
     if (!getDocName()) {
-        document.location.href = '#' + ( new Date().getTime() ).toString(32);
+        navigateToNewFile();
     }
 
     var apiServices = {
-        preDoc: {  }
-        , getDocument: function(name, done) {
-            if(this.writeXHR){
+        preDoc: {}
+        , getDocument: function (name, done) {
+            if (this.writeXHR) {
                 this.writeXHR.abort();
                 this.writeXHR = null;
             }
@@ -22,34 +27,35 @@
             this.writeXHR = $.ajax({
                 url: '/api/get/' + encodeURIComponent(name)
                 , dataType: 'json'
-                , success: function(res) {
+                , success: function (res) {
                     done(res);
                     self.preDoc = res;
                 }
-                , error: function() {
+                , error: function () {
                     self.preDoc = {};
                     done({});
                 }
             });
         }
-        , saveDocument: function(doc, done) {
-            done = done || function(){};
+        , saveDocument: function (doc, done) {
+            done = done || function () {
+                };
             var self = this;
-            this.saveQ.func(function(next) {
+            this.saveQ.func(function (next) {
                 self._saveDocument(doc, next);
-            }).func(function(){
+            }).func(function () {
                 done();
             });
         }
-        , _saveDocument: function(doc, done) {
-            if(this.readXHR){
+        , _saveDocument: function (doc, done) {
+            if (this.readXHR) {
                 this.readXHR.abort();
                 this.readXHR = null;
             }
             var self = this;
             self.windowLeaveMessage = 'Saving content';
             var content = doc.content;
-            if(content && content !== this.preDoc.content) {
+            if (content && content !== this.preDoc.content) {
                 this.preDoc.content = doc.content;
                 this.writeXHR = $.ajax({
                     url: '/api/edit'
@@ -61,18 +67,18 @@
                     }
                     , type: 'post'
                     , success: function (res) {
-                        if(!doc._preDoc._id) {
+                        if (!doc._preDoc._id) {
                             doc._preDoc._id = res._id;
                         }
                         self.windowLeaveMessage = null;
                         done()
                     }
-                    , error: function() {
+                    , error: function () {
                         self.windowLeaveMessage = 'Failed to save';
                         done();
                     }
                 })
-            }else{
+            } else {
                 done();
             }
         }
@@ -83,40 +89,38 @@
         , windowLeaveMessage: null
     };
 
-
-
-    window.onbeforeunload = function() {
-        if(apiServices.saveQ.length > 0 && apiServices.windowLeaveMessage){
+    window.onbeforeunload = function () {
+        if (apiServices.saveQ.length > 0 && apiServices.windowLeaveMessage) {
             return apiServices.windowLeaveMessage;
         }
     };
 
-    $(function() {
+    $(function () {
         var $editor = $('#editor')
             , editor = $editor.data('editor')
             , $textarea = $editor.find('textarea')
             , $win = $(window)
             , _preDocName = getDocName()
-            , loadDocument = function(name) {
+            , loadDocument = function (name) {
                 $('title').html(name + ' - Yindoc');
                 toggleLoadingMode(true);
-                apiServices.getDocument(name, function(res) {
+                apiServices.getDocument(name, function (res) {
                     editor.setValue(res.content);
                     editor.clearSelection();
                     toggleLoadingMode(false);
                 });
             }
-            , toggleLoadingMode = function(loadingMode) {
-                if(loadingMode) {
+            , toggleLoadingMode = function (loadingMode) {
+                if (loadingMode) {
                     $('.x-hide-onready').show();
-                }else{
+                } else {
                     $('.x-hide-onready').hide();
                 }
             };
 
         loadDocument(_preDocName);
 
-        $textarea.keyup(function() {
+        $textarea.keyup(function () {
             apiServices.saveDocument({
                 name: getDocName()
                 , content: editor.getValue()
@@ -124,14 +128,16 @@
             });
         });
 
-        $win.hashchange(function() {
+        $win.hashchange(function () {
             var docName = getDocName();
-            if(!apiServices.isRename && _preDocName != docName) {
-                _preDocName = docName;
-                loadDocument(docName);
+            if (!docName) {
+                navigateToNewFile();
+            } else {
+                if (!apiServices.isRename && _preDocName != docName) {
+                    _preDocName = docName;
+                    loadDocument(docName);
+                }
             }
         });
-
-
     });
 })(jQuery);
